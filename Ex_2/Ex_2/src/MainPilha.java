8eb31f2f6067dd4f795c922d87f6db2649ebcaa3
patrickmatos9 <81051549+patrickmatos9@users.noreply.github.com>
2022-10-26 15:00:16 -0300

    Update MainPilha.java

--- Ex_2/Ex_2/src/MainPilha.java
+++ Ex_2/Ex_2/src/MainPilha.java
@@ -3,10 +3,22 @@ public class MainPilha {
         Pilha p1 = new Pilha(5);
         p1.push('A');
         p1.push('B');
+        p1.push('A');
+        p1.push('C');
+        p1.push('B');
+        p1.push('B');
+        p1.push('A');
+        p1.push('D');
+        p1.push('A');
+        p1.push('B');
         p1.push('C');
         p1.push('B');
         p1.push('A');
-        p1.push('D'); // Aqui verificar a pilha!
+        p1.push('D');
+
+
+
+       // p1.push('D'); // Aqui verificar a pilha!
 
         
     }
