91f3c2799e50458c4ee4c34e2e10e714408f8d0b
patrickmatos9 <81051549+patrickmatos9@users.noreply.github.com>
2022-10-25 21:55:47 -0300

    Update Main.java

--- Ex_1/src/Main.java
+++ Ex_1/src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args){
-        Pilha p1 = new Pilha();
-        p1.setTam();
-        for(int i = 0; i < p1.tam; i++){
-            p1.push(); // Pilha toda preenchida
-        }
-        p1.preencheX();
-        p1.preencheY();
-        p1.comparaXY();
+        Pilha p_principal = new Pilha(4);
+        //Pilha p_auxiliar = new Pilha(p_principal.getTam() / 2); // Auxiliar precisa ter a metade inteira do Tam 
+        p_principal.push('A');
+        p_principal.push('B');
+        p_principal.push('C');
+        p_principal.push('B');
+        p_principal.push('A');
+
+
 
     }
 }
