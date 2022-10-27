c36defde897e11ba5f28eff777670ece57ce7026
patrickmatos9 <81051549+patrickmatos9@users.noreply.github.com>
2022-10-27 14:01:25 -0300

    Add Ex_3

---
+++ Ex_3/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        Pilha p1 = new Pilha(10);
+        p1.push_p2('D');
+        p1.push_p2('A');
+        p1.push_p2('N');
+        p1.push_p2('I');
+        p1.push_p2('E');
+        p1.push_p2('L');
+        p1.push_p1('P');
+        p1.push_p1('T');
+        p1.push_p1('K');
+        p1.push_p1('S');
+        p1.printPilha();
+
+        
+       
+
+    }
+}
---
+++ Ex_3/src/Pilha.java
@@ -0,0 +1,71 @@
+public class Pilha {
+    int tam;
+    char vet[];
+    int topo_p1;
+    int topo_p2;
+
+    public Pilha(int tam){
+        this.tam = tam;
+        this.topo_p1 = -1;
+        this.topo_p2 = tam;
+        this.vet = new char[this.tam];
+    }
+    private boolean cheia(){
+        if(topo_p1 == tam - 1 || topo_p2 == 0)
+            return true;
+        else
+            return false;
+    }
+    private boolean vazia(){
+        if(topo_p1 == -1 && topo_p2 == tam)
+            return true;
+        else
+            return false;
+    }
+    public char pop_p1(){
+        char c = '\0';
+        if(!vazia()){
+            c = vet[topo_p1];
+            topo_p1--;
+        }
+        else
+            System.out.println("A pilha está vazia!");
+        return c;
+    }
+    public boolean push_p1(char elemento){
+        if(!cheia()){
+            topo_p1++;
+            vet[topo_p1] = elemento;
+            return true;
+        }
+        else{
+            System.out.println("A pilha está cheia!");
+            return false;
+        }
+    }
+    public char pop_p2(){
+        char c = '\0';
+        if(!vazia()){
+            c = vet[topo_p2];
+            topo_p2++;
+        }
+        else
+            System.out.println("A pilha está vazia!");
+        return c;
+    }
+    public boolean push_p2(char elemento){
+        if(!cheia()){
+            topo_p2--;
+            vet[topo_p2] = elemento;
+            return true;
+        }
+        else{
+            System.out.println("A pilha está cheia!");
+            return false;
+        }
+    }
+    public void printPilha(){
+        for(int i = 0; i < tam; i++)
+            System.out.println(vet[i]);
+    }
+}
